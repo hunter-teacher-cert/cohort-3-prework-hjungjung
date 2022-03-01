20abbe63253da8b86f4d3649ba5d14fe132cca0a
hjungjung <97001868+hjungjung@users.noreply.github.com>
2022-03-01 02:56:59 +0000

    This is Pre-work2 Date

---
+++ pre02/Date.java
@@ -0,0 +1,25 @@
+public class Date {
+  public static void main(String[] args){
+    System.out.println("<This is the prepwork 2>");
+
+String dayOfWeek;
+String month;
+int dayOfMonth;
+int year;
+
+dayOfWeek = "Monday";
+month = "February";
+dayOfMonth = 28;
+year = 2022;
+
+System.out.println("American format:");
+System.out.println("Today is "+ dayOfWeek + ", " + month + " " + dayOfMonth + "," + year + ".");
+
+System.out.println("European format:");
+System.out.println("Today is" + " " + dayOfWeek + " " + dayOfMonth + " " + month + " " + year);
+
+    
+    
+  }
+}
+
